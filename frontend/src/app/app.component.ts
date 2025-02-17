113d7d6cf2bfbf2ec06bcf67c8bf75bfbf4363eb
joaoefornazari <joaoefornazari@gmail.com>
2025-02-17 14:52:17 -0300

    feat: componentizing HTML templates

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,10 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  imports: [],
-  template: `
-    <h1>Default</h1>
-  `,
-  styleUrls: ['./app.component.css'],
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'default';
+	title = 'calendar';
 }
