ca5620c3a0917893f83bfe1306e030d655d724d3
joaoefornazari <joaoefornazari@gmail.com>
2025-02-18 14:18:48 -0300

    fix: adjusting Express types

---
+++ backend/routes/events.ts
@@ -0,0 +1,31 @@
+import { Router } from 'express';
+const router = Router();
+
+// ROUTE: /events
+ 
+/* GET events listing. */
+router.get('/', function(req, res, next) {
+  
+});
+
+/* GET specific event. */
+router.get('/:id', function(req, res, next) {
+  
+});
+
+/* POST create event. */
+router.post('/', function(req, res, next) {
+	
+});
+
+/* PUT update event. */
+router.put('/:id', function(req, res, next) {
+	
+});
+
+/* DELETE event. */
+router.delete('/:id', function(req, res, next) {
+	
+});
+
+export default router;
