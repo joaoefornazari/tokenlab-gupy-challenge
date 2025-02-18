0a8a6322f57b54e355b701017445e5ff44971306
joaoefornazari <joaoefornazari@gmail.com>
2025-02-18 14:28:35 -0300

    fix: adjusting files to fit type checker demands

---
+++ backend/app.ts
@@ -0,0 +1,44 @@
+import Express, { Request, Response, NextFunction } from 'express';
+
+import createError from 'http-errors';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
+import eventsRouter from './routes/events';
+
+const app = Express();
+
+app.use(logger('dev'));
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(Express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/events', eventsRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+interface HTTPError extends Error {
+	status?: number;
+}
+
+// error handler
+app.use(function(err: HTTPError, req: Request, res: Response, next: NextFunction) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
