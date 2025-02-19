8210cfada7a95a0ab1e723b7c9806fa64b975614
joaoefornazari <joaoefornazari@gmail.com>
2025-02-19 13:37:17 -0300

    fix: including file extension in import path

--- backend/src/http/models/event.ts
+++ backend/src/http/models/event.ts
@@ -1,5 +1,5 @@
 import { DataTypes } from 'sequelize';
-import sequelize from '../../database/database';
+import sequelize from '../../database/database.js';
 
 const Event = sequelize.define(
 	'Event',
