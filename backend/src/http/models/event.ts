819243cb582a780623c9e29d46d8df24e3347875
joaoefornazari <joaoefornazari@gmail.com>
2025-02-18 22:41:40 -0300

    feat: new Event model

---
+++ backend/src/http/models/event.ts
@@ -0,0 +1,38 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../../database/database';
+
+sequelize.define(
+	'Event',
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		description: {
+			type: DataTypes.TEXT('medium'),
+			allowNull: false,
+		},
+		start_datetime: {
+			type: DataTypes.DATE,
+			allowNull: false,
+		},
+		end_datetime: {
+			type: DataTypes.DATE,
+			allowNull: false,
+		},
+		content: {
+			type: DataTypes.TEXT('long'),
+			allowNull: false,
+		},
+		/*
+		timezone: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		*/
+	},
+	{
+		tableName: 'events',
+	}
+);
