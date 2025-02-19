a830a5ff491ebcdef5fdd6d05832cbe14a9a6435
joaoefornazari <joaoefornazari@gmail.com>
2025-02-19 12:45:35 -0300

    feat: event routes

--- backend/routes/events.ts
+++ backend/routes/events.ts
@@ -1,31 +1,25 @@
 import { Router } from 'express';
 const router = Router();
 
+import EventController from '../src/http/controllers/eventController';
+import EventService from '../src/http/services/eventService';
+
 // ROUTE: /events
+const eventController = new EventController(new EventService());
  
 /* GET events listing. */
-router.get('/', function(req, res, next) {
-  
-});
+router.get('/', eventController.get);
 
 /* GET specific event. */
-router.get('/:id', function(req, res, next) {
-  
-});
+router.get('/:id', eventController.getById);
 
 /* POST create event. */
-router.post('/', function(req, res, next) {
-	
-});
+router.post('/', eventController.create);
 
 /* PUT update event. */
-router.put('/:id', function(req, res, next) {
-	
-});
+router.put('/:id', eventController.update);
 
 /* DELETE event. */
-router.delete('/:id', function(req, res, next) {
-	
-});
+router.delete('/:id', eventController.delete);
 
 export default router;
