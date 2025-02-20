b59773776da791bbe0a3545cc9d0174e568c2549
joaoefornazari <joaoefornazari@gmail.com>
2025-02-20 20:51:02 -0300

    feat: main container of whole web app

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import {Component} from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
+	imports: [CommonModule, RouterOutlet],
 	styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
