78e0c60969ad738b5fec6888f4f353b989ff1f44
joaoefornazari <joaoefornazari@gmail.com>
2025-02-20 21:26:06 -0300

    feat: using AppComponent as standalone
    Angular 18+ guidelines

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
 	selector: 'app-root',
+	standalone: true,
 	templateUrl: './app.component.html',
 	imports: [CommonModule, RouterOutlet],
 	styleUrls: ['./app.component.css'],
