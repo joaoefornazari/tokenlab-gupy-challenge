d147bf9fb08a6c1871b373a3bdf77047bdae136c
joaoefornazari <joaoefornazari@gmail.com>
2025-02-20 21:41:13 -0300

    feat: starting calendar layout design

--- frontend/src/app/calendar/layout/layout.component.ts
+++ frontend/src/app/calendar/layout/layout.component.ts
@@ -1,14 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent as CalendarHeader } from './header/header.component';
 
 @Component({
-  selector: 'app-layout',
-  imports: [],
-  template: `
-    <p>
-      layout works!
-    </p>
-  `,
-  styles: ``
+  selector: 'calendar-layout',
+	standalone: true,
+  imports: [CommonModule, RouterOutlet, CalendarHeader],
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.css'],
 })
 export class LayoutComponent {
 
