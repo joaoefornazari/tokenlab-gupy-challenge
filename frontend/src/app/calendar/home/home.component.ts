64cfcfcd50ce3cfd788cbbdd585122c4b3784035
joaoefornazari <joaoefornazari@gmail.com>
2025-02-21 15:23:13 -0300

    feat: calendar home v0

--- frontend/src/app/calendar/home/home.component.ts
+++ frontend/src/app/calendar/home/home.component.ts
@@ -1,15 +1,63 @@
 import { Component } from '@angular/core';
+import { ListComponent as CalendarList } from './list/list.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
-  template: `
-    <p>
-      home works!
-    </p>
-  `,
-  styles: ``
+  imports: [CalendarList],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+	private today = new Date()
+	protected current: Date
+
+	constructor() {
+		this.current = this.today
+	}
+
+	public getTodayMonth() {
+		return this.today.getMonth()
+	}
+
+	public getTodayDay() {
+		return this.today.getDay()
+	}
+
+	public getTodayYear() {
+		return this.today.getFullYear()
+	}
+
+	public getCurrentMonth() {
+		return this.current.getMonth()
+	}
+
+	public getCurrentDay() {
+		return this.current.getDay()
+	}
+
+	public getCurrentYear() {
+		return this.current.getFullYear()
+	}
+
+	public changeCurrentMonth(month: 'previous' | 'next' | number) {
+		const newMonth = (month === 'previous' ? this.current.getMonth() - 1 : this.current.getMonth() + 1)
+		this.current.setMonth(newMonth)
+		return 
+	}
+
+	public changeCurrentYear(year: number) {
+		this.current.setFullYear(year)
+		return
+	}
+
+	public changeCurrentDay(day: number) {
+		this.current.setDate(day)
+		return
+	}
+
+	public backToToday() {
+		this.current = this.today
+		return
+	}
 
 }
