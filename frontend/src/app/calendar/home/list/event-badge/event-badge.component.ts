335c6271bef2e07ced06ea8e85e916becdf00bd0
joaoefornazari <joaoefornazari@gmail.com>
2025-02-22 14:19:49 -0300

    feat: event badge methods and imports to template

--- frontend/src/app/calendar/home/list/event-badge/event-badge.component.ts
+++ frontend/src/app/calendar/home/list/event-badge/event-badge.component.ts
@@ -1,15 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { NgIf } from '@angular/common'
+
+type CalendarEvent = {
+	description: string
+	start_datetime: string
+	end_datetime: string
+	content?: string
+}
 
 @Component({
-  selector: 'app-event-badge',
-  imports: [],
-  template: `
-    <p>
-      event-badge works!
-    </p>
-  `,
-  styles: ``
+  selector: 'calendar-event-badge',
+  imports: [NgIf],
+  templateUrl: './event-badge.component.html',
+  styleUrls: ['./event-badge.component.css']
 })
-export class EventBadgeComponent {
+export class EventBadgeComponent implements OnChanges {
+	@Input() public event!: CalendarEvent
+	
+	private description: string = ""
+	private start: string= ""
+	private end: string = ""
+	private content: string = ""
+
+	private expanded: boolean = false
+
+	constructor() {}
+
+	ngOnChanges(changes: SimpleChanges): void {
+		if (changes['event']) {
+			this.setEventFields(this.event)
+		}
+	}
+
+	public setEventFields(event: CalendarEvent): void {
+		this.setDescription(event.description)
+		this.setStart(event.start_datetime)
+		this.setEnd(event.end_datetime)
+		if (event.content) this.setContent(event.content)
+	}
+
+	public setDescription(description: string): void {
+		this.description = description
+	}
+
+	public setStart(start: string): void {
+		this.start = new Date(start).toLocaleTimeString('pt', {
+			hour: '2-digit',
+			minute: '2-digit',
+			second: undefined,
+		})
+	}
+
+	public setEnd(end: string): void {
+		this.end = new Date(end).toLocaleTimeString('pt', {
+			hour: '2-digit',
+			minute: '2-digit',
+			second: undefined,
+		})
+	}
+
+	public setContent(content: string): void {
+		this.content = content
+	}
+
+	public getDescription(): string {
+		return this.description
+	}
+
+	public getStart(): string {
+		return this.start
+	}
+
+	public getEnd(): string {
+		return this.end
+	}
+
+	public isExpanded(): boolean {
+		return this.expanded
+	}
+
+	public toggleExpanded(): void {
+		this.expanded = !this.expanded
+	}
+
+	public hasContent(): boolean {
+		return this.content.length > 0
+	}
+
+	public getContent(): string {
+		return this.content
+	}
 
 }
