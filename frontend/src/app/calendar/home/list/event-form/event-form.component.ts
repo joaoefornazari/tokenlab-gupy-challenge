b2cdffc80eebdc9a074ee0bbc4c04f83d912479d
joaoefornazari <joaoefornazari@gmail.com>
2025-02-22 21:38:30 -0300

    feat: event form design

--- frontend/src/app/calendar/home/list/event-form/event-form.component.ts
+++ frontend/src/app/calendar/home/list/event-form/event-form.component.ts
@@ -1,11 +1,52 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { CalendarEvent } from 'src/types';
 
 @Component({
-  selector: 'calendar-event-form',
-  imports: [],
-  templateUrl: './event-form.component.html',
-  styleUrls: ['./event-form.component.css'],
+    selector: 'calendar-event-form',
+		standalone: true,
+    templateUrl: './event-form.component.html',
+    styleUrls: ['./event-form.component.css'],
+    imports: [NgIf]
 })
 export class EventFormComponent {
+    public event: CalendarEvent = {
+        description: '',
+        start_datetime: '',
+        end_datetime: '',
+        content: ''
+    };
 
+    private addingEvent: boolean = false;
+
+    constructor() {}
+
+    public isAddingEvent(): boolean {
+        return this.addingEvent;
+    }
+
+    public startAddingEvent(): void {
+        this.addingEvent = true;
+    }
+
+    public cancelAddingEvent(): void {
+        this.addingEvent = false;
+        this.resetForm();
+    }
+
+    public onSubmit(): void {
+        console.log(this.event);
+        // Handle form submission, e.g., send the payload to the server
+        this.addingEvent = false;
+        this.resetForm();
+    }
+
+    public resetForm(): void {
+			this.event = {
+					description: '',
+					start_datetime: '',
+					end_datetime: '',
+					content: ''
+			};
+    }
 }
