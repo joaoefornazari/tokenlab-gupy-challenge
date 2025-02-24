98ec16631bb623fa039b3d5ca26187db88831c1e
joaoefornazari <joaoefornazari@gmail.com>
2025-02-24 13:10:24 -0300

    feat: create event integrated with API

--- frontend/src/app/calendar/home/list/event-form/event-form.component.ts
+++ frontend/src/app/calendar/home/list/event-form/event-form.component.ts
@@ -1,11 +1,14 @@
-import { Component, Input } from '@angular/core';
-import { NgIf } from '@angular/common';
-import { CalendarEvent } from 'src/types';
+import { Component, Input } from '@angular/core'
+import { NgIf } from '@angular/common'
+import { CalendarEvent } from 'src/types'
+import { ApiService } from 'src/app/services/api.service'
+import { Output, EventEmitter } from '@angular/core'
+import { FormsModule } from '@angular/forms'
 
 @Component({
     selector: 'calendar-event-form',
 		standalone: true,
-    imports: [NgIf],
+    imports: [NgIf, FormsModule],
     templateUrl: './event-form.component.html',
     styleUrls: ['./event-form.component.css'],
 })
@@ -15,10 +18,10 @@ export class EventFormComponent {
         start_datetime: '',
         end_datetime: '',
         content: ''
-    };
+    }
 
 		@Input()
-		public day!: number
+		public day!: any
 		
 		@Input()
 		public month!: number
@@ -26,47 +29,57 @@ export class EventFormComponent {
 		@Input()
 		public year!: number
 
-    private addingEvent: boolean = false;
+    private addingEvent: boolean = false
+		private api: ApiService
 
-    constructor() {}
+    constructor() {
+			this.api = new ApiService()
+		}
 
     public isAddingEvent(): boolean {
-        return this.addingEvent;
+        return this.addingEvent
     }
 
     public startAddingEvent(): void {
-        this.addingEvent = true;
+        this.addingEvent = true
     }
 
     public cancelAddingEvent(): void {
-        this.addingEvent = false;
-        this.resetForm();
+        this.addingEvent = false
+        this.resetForm()
     }
 
-    public onSubmit(): void {
-				this.event.start_datetime = this.convertToISOString(this.event.start_datetime)
-				this.event.end_datetime = this.convertToISOString(this.event.end_datetime)
-
-				// send to server
-        this.addingEvent = false;
-        this.resetForm();
-    }
+		@Output()
+		public eventCreated = new EventEmitter<void>()
 
 		private convertToISOString(time: string): string {
 			const date = new Date()
-
 			date.setMonth(this.month)
-			date.setDate(this.day)
-			date.setFullYear(this.year)
-			
+			date.setDate(this.day.day)
 			const [hours, minutes] = time.split(':').map(Number)
-
 			date.setHours(hours)
 			date.setMinutes(minutes)
 			date.setSeconds(0)
 			date.setMilliseconds(0)
-
 			return date.toISOString()
+		}
+
+		public onSubmit(): void {
+			this.event.start_datetime = this.convertToISOString(this.event.start_datetime);
+			this.event.end_datetime = this.convertToISOString(this.event.end_datetime);
+
+			this.api.post('/calendar/events', this.event)
+				.then((res) => {
+					this.eventCreated.emit()
+				})
+				.catch((error) => {
+					alert('Error while creating event')
+					console.error(error)
+				})
+				.finally(() => {
+					this.addingEvent = false
+					this.resetForm()
+				})
 		}
 
     public resetForm(): void {
@@ -75,6 +88,6 @@ export class EventFormComponent {
 					start_datetime: '',
 					end_datetime: '',
 					content: ''
-			};
+			}
     }
 }
