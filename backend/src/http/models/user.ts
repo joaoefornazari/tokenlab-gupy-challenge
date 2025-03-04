d7fee9b9080dbd7dc065788e820d3a5d1daca393
joaoefornazari <joaoefornazari@gmail.com>
2025-03-04 14:19:59 -0300

    feat: Sequelize models for Login context

---
+++ backend/src/http/models/user.ts
@@ -0,0 +1,36 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../../database/database.ts';
+
+const User = sequelize.define(
+	'Users',
+	{
+		id: {
+			type: DataTypes.UUIDV4,
+			primaryKey: true,
+			allowNull: false,
+		},
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+		},
+		password: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+		},
+		createdAt: {
+			type: DataTypes.DATE,
+			allowNull: false,
+			defaultValue: DataTypes.NOW,
+		},
+		updatedAt: {
+			type: DataTypes.DATE,
+			allowNull: true,
+		},
+	},
+	{
+		tableName: 'users',
+	}
+)
+
+export default User;
