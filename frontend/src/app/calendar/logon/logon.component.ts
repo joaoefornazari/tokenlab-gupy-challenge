6d072a64cf9d1f3c7fd45a017684f06b66d5e862
joaoefornazari <joaoefornazari@gmail.com>
2025-03-05 14:05:05 -0300

    feat: logon UI flow

--- frontend/src/app/calendar/logon/logon.component.ts
+++ frontend/src/app/calendar/logon/logon.component.ts
@@ -1,11 +1,51 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ApiService } from 'src/app/services/api/api.service';
 
 @Component({
   selector: 'calendar-logon',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './logon.component.html',
 	styleUrls: ['./logon.component.css']
 })
 export class LogonComponent {
+	public formData: {
+		email: string
+		name: string
+		password: string
+		confirmPassword: string
+	}
 
+	private api: ApiService
+
+	constructor() {
+		this.formData = {
+			email: '',
+			name: '',
+			password: '',
+			confirmPassword: ''
+		}
+		this.api = new ApiService()
+	}
+
+	public onCancel() {
+		window.location.href = '/calendar/login'
+	}
+
+	public async onSubmit() {
+		try {
+			if (this.formData.password !== this.formData.confirmPassword) {
+				throw new Error('Passwords don\'t match.')
+			}
+
+			const result = await this.api.post('/calendar/users', this.formData)
+			if (result.status === 'rejected') {
+				throw new Error(result.reason.response.data.error)
+			}
+			window.location.href = '/calendar/login'
+		
+		}	catch (error: any) {
+			alert(error)
+		}	
+	}
 }
