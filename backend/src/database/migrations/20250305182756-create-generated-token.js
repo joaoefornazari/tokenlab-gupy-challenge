cfdd4e3de28b2e87e4b6d1e57185a1a8d635608a
joaoefornazari <joaoefornazari@gmail.com>
2025-03-05 15:47:06 -0300

    feat: creating generated token table to safer auth process

---
+++ backend/src/database/migrations/20250305182756-create-generated-token.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const { DataTypes } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+const createGeneratedToken = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('generated-tokens', {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+      },
+      token: {
+        type: DataTypes.STRING
+      },
+      userId: {
+        type: DataTypes.UUID
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+				defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('generated-tokens');
+  }
+};
+
+export default createGeneratedToken;
